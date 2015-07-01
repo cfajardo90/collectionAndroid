8a225c95c882b2e134289ebd6662941b7778696e
Cesar Fajardo <cfajardoramos@gmail.com>
2015-07-01 10:34:24 -0500

    Personalizacion nivel 1

--- app/src/main/java/com/example/mobilelab3/mundo/hola/collection2/MainActivity.java
+++ app/src/main/java/com/example/mobilelab3/mundo/hola/collection2/MainActivity.java
@@ -4,12 +4,17 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private ListView listView;
+    private String colores[];
+    private ListAdapter listAdapterColores;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,11 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         // Obtener el objecto de listView
         listView = (ListView) findViewById(R.id.listColores);
+        colores = getResources().getStringArray(R.array.colores);
+        //
+        listAdapterColores = new ArrayAdapter<String>(this,
+                                                android.R.layout.simple_list_item_1, colores);
+        listView.setAdapter(listAdapterColores);
     }
 
 }
